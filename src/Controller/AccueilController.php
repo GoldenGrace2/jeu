86db71f66ad5a0bccc43b40d04bc8386ff76246e
GoldenGrace2 <37578645+GoldenGrace2@users.noreply.github.com>
2020-04-06 23:43:51 +0200

    Fix bad repo

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,17 +7,20 @@ use App\Entity\User;
 use App\Entity\Partie;
 use App\Entity\Chat;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 use App\Repository\CarteRepository;
 use App\Repository\CasesRepository;
 use App\Repository\JouerRepository;
 use App\Repository\UserRepository;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
@@ -26,6 +29,7 @@ class AccueilController extends AbstractController
      */
     public function index()
     {
+        
         $em = $this->getDoctrine()->getManager();
 
         $partie = $em->getRepository(Partie::class);
