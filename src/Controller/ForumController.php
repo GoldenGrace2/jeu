8e3ba019b39ce8a599422f26f584284f12ddfd76
GoldenGrace2 <37578645+GoldenGrace2@users.noreply.github.com>
2020-05-01 04:38:22 +0200

    Fix bad repo

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -1,9 +1,24 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Forum;
+use App\Entity\User;
+use App\Form\PostForumType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 /**
 * @Route("/forum")
@@ -16,18 +31,62 @@ class ForumController extends AbstractController
     */
     public function index()
     {
+
+        $repository = $this->getDoctrine()->getRepository(Forum::class);
+        $forum = $repository->findAll();
+
+
         return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
+            'forum' => $forum,
+            'user' => $this->getUser()
         ]);
     }
 
-       /**
-     * @Route("/forum_post", name="app_forum_post")
+    /**
+     * @Route("/post/{forum}", name="post_unique")
+     * @param Forum $forum
+     *
+     * @return Response
      */
-    public function post()
+    public function postunique(EntityManagerInterface $entityManager, Forum $forum)
     {
+        $repository = $this->getDoctrine()->getRepository(Forum::class);
+
+        $forumunique = $repository->findOneBy(
+            ['id' => $forum],
+        );
+        return $this->render('forum/unique.html.twig', [
+            'forum' => $forumunique
+        ]);
+    }
+
+    
+    /**
+    * @Route("/creerpost", name="post_topic")
+    */
+    public function post(Request $request): Response
+    {    
+  
+        $forum = new Forum();
+        $form = $this->createForm(PostForumType::class, $forum);
+        $form->handleRequest($request);
+        $date = new \DateTime('@'.strtotime('now'));
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $forum->setAuteur($this->getUser());
+            $forum->setDate($date);
+
+         
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($forum);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+        }
         return $this->render('forum/post.html.twig', [
-            'controller_name' => 'ForumController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/PostForumType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Forum;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class PostForumType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('category', ChoiceType::class, [
+            'choices'  => [
+                'Bugs' => 'Bugs',
+                'Fun' => 'Fun',
+            ],
+        ])
+            ->add('Message')
+            ->add('Titre')
+            ->add('save', SubmitType::class, ['label' => 'Confirmer'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Forum::class,
+        ]);
+    }
+}
